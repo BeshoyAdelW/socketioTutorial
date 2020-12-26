450aadaddd847c03759ea633f760162a82e08ff6
Beshoy Adel <Beshoy.adelwaheeb@yahoo.com>
2020-12-26 12:34:23 +0200

    Setup Socket.io and listening to connections

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const app = express();
 
 //App Setup
+const app = express();
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
@@ -9,3 +9,10 @@ const server = app.listen(port, () =>
 
 //Static files
 app.use(express.static("public"));
+
+//Socket.io setup
+var io = require("socket.io")(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+});
